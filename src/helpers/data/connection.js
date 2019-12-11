a9d20846f04fc1770f64c3183f774705c1faa7b4
Maria <mariabrock414@gmail.com>
2019-12-10 21:34:28 -0600

    can log in

--- src/helpers/data/connection.js
+++ src/helpers/data/connection.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase/app';
 import firebaseConfig from '../apiKeys.json';
 
-const firebaseApps = () => {
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig.firebaseKeys);
-    }
+const firebaseApp = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig.firebaseKeys);
+  }
 };
 
 export default firebaseApp;
