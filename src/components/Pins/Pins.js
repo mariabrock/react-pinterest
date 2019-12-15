d40af53f6a2ae8622ba55dd89bc117eabaf0ae01
Maria <mariabrock414@gmail.com>
2019-12-14 20:14:55 -0600

    wip

--- src/components/Pins/Pins.js
+++ src/components/Pins/Pins.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import pinShape from '../../helpers/propz/pinShape';
 
 class Pins extends React.Component {
@@ -13,24 +13,24 @@ class Pins extends React.Component {
     setSelectedPinId = (e) => {
       e.preventDefault();
       const { setSinglePin, pin } = this.props;
-      setSinglePin();
+      setSinglePin(pin.id);
     }
 
     render() {
       const { pin } = this.props;
+
       return (
         <div className="Pin col-3">
         <div className="card">
           <img src={pin.imageUrl} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title">{pin.title}</h5>
-            <button className="btn btn-danger" onClick={this.setSelectedBoardId}>X</button>
+            <button className="btn btn-danger" onClick={this.setSelectedPinId}>X</button>
           </div>
         </div>
       </div>
       );
     }
 }
-
 
 export default Pins;
