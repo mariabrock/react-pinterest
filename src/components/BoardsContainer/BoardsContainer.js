7a69b333335d2332e4b055042745476de73d1f47
Maria <mariabrock414@gmail.com>
2019-12-17 20:27:27 -0600

    can add a new board

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 import authData from '../../helpers/data/authData';
 import boardData from '../../helpers/data/boardData';
@@ -16,17 +17,34 @@ state ={
 }
 
 componentDidMount() {
-  boardData.getBoardsByUid(authData.getUid())
-    .then((boards) => {
-      this.setState({ boards });
-    })
-    .catch((errFromBoardsContainer) => console.error({ errFromBoardsContainer }));
+  this.getBoards();
 }
 
-render() {
-  const { setSingleBoard } = this.props;
-  return (<div>{this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard={setSingleBoard} />))}</div>);
-}
+  getBoards = () => {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => {
+        this.setState({ boards });
+      })
+      .catch((errFromBoardsContainer) => console.error({ errFromBoardsContainer }));
+  }
+
+  addBoard = (newBoard) => {
+    boardData.saveBoard(newBoard)
+      .then(() => {
+        this.getBoards();
+      })
+      .catch((errorFromSaveBoard) => console.error({ errorFromSaveBoard }));
+  }
+
+  render() {
+    const { setSingleBoard } = this.props;
+
+    return (
+    <div>
+      <BoardForm addBoard={this.addBoard} />
+      {this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard={setSingleBoard} />))}
+      </div>);
+  }
 }
 
 export default BoardsContainer;
